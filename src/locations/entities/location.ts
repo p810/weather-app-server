ee41011a6723e5957d45d0a03a507d3c99e6811e
Payton Bice <hellopayton@gmail.com>
2020-08-02 16:05:44 -0500

    feat: Updates location.ts and test cases - Updated import from JSON file to TypeScript file (1906289) - Changed getRegionByCode() to RegionByCode() for consistency with other entity functions - Code style changes: Updated each entity function to use a named function expression, per the AirBnB style guide, for increased visibility in the debugger and stack traces

--- src/locations/entities/location.ts
+++ src/locations/entities/location.ts
@@ -1,23 +1,39 @@
-import { codeToName } from './states.json';
+import { codeToState } from './states';
 
-export const Region = (name: string, code: string) => ({ name, code });
+export const Region = function getRegion(name: string, code: string) {
+    return {
+        name,
+        code,
+    }
+}
 
-export const getRegionByCode = (code: string) => Region(codeToName[code], code);
+export const RegionByCode = function getRegionByCode(code: string) {
+    return Region(codeToState[code], code);
+}
 
 export interface RegionModel extends ReturnType<typeof Region> {}
 
-export const Location = (
+export const Location = function getLocationModel(
     city: string,
     state: RegionModel,
     zip: string | number,
     coordinates: CoordinatesModel
-) => ({ city, state, zip, coordinates });
+) {
+    return {
+        city,
+        state,
+        zip,
+        coordinates,
+    }
+}
 
 export interface LocationModel extends ReturnType<typeof Location> {}
 
-export const Coordinates = (
-    latitude: number | string,
-    longitude: number | string
-) => ({ latitude, longitude });
+export const Coordinates = function getCoordinates(latitude: number | string, longitude: number | string) {
+    return {
+        latitude,
+        longitude,
+    }
+}
 
 export interface CoordinatesModel extends ReturnType<typeof Coordinates> {}
--- tests/entities/region.spec.ts
+++ tests/entities/region.spec.ts
@@ -1,7 +1,7 @@
-import { getRegionByCode } from '../../src/locations/entities/Location';
+import { RegionByCode } from '../../src/locations/entities/Location';
 
 it('should return a region object with Alabama as its state', () => {
-    const alabama = getRegionByCode('AL');
+    const alabama = RegionByCode('AL');
 
     expect(alabama.name).toBe('Alabama');
 });
