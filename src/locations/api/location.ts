0fdb21807fc85591cce5ce98a5492aea770d59b4
Payton Bice <hellopayton@gmail.com>
2020-08-02 15:55:57 -0500

    feat: Refactors LocationApiClient, adds controllers - LocationApiClient was changed to an abstract class to reduce boilerplate (the constructor signature) - Added zipController() and coordinateController() for use with ResponderFactory and Express

--- src/locations/api/location.ts
+++ src/locations/api/location.ts
@@ -1,8 +1,50 @@
-import { ApiClient } from '../../http';
+import {
+    ApiClient,
+    HttpAdapter,
+    ApiConfig,
+    ApiResult,
+    ApiResultObject,
+} from '../../http';
 import { LocationModel } from '../entities/location';
+import { Request } from 'express';
 
-export default interface LocationApiClient extends ApiClient {
-    getLocationByZip(zip: string | number): Promise<LocationModel>;
+export abstract class LocationApiClient implements ApiClient {
+    constructor(
+        readonly adapter: HttpAdapter,
+        readonly config?: ApiConfig,
+    ) {}
 
-    getLocationByCoordinates(lat:  string | number, lng: string | number): Promise<LocationModel>;
+    abstract getLocationByZip(zip: string | number): Promise<LocationModel>;
+
+    abstract getLocationByCoordinates(lat:  string | number, lng: string | number): Promise<LocationModel>;
+}
+
+export const zipController = function handleZipRequest(
+    client: LocationApiClient,
+    req: Request,
+): Promise<ApiResultObject> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let response = await client.getLocationByZip(req.params.zip);
+
+            resolve(ApiResult(response));
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+export const coordinateController = function handleCoordinateRequest(
+    client: LocationApiClient,
+    req: Request,
+): Promise<ApiResultObject> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let response = await client.getLocationByZip(req.params.zip);
+
+            resolve(ApiResult(response));
+        } catch (e) {
+            reject(e);
+        }
+    });
 }
