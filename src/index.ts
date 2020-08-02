2e2295a2f0a7a12c9c576f5ab6a08edc14c0e563
Payton Bice <hellopayton@gmail.com>
2020-08-02 16:19:33 -0500

    feat: Polyfills fetch(), adds ResponderFactory/Logger objects

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import { ResponderFactory, Domains } from './routes';
+import { ConsoleLogger, StackLogger } from './util/log';
+import { clientMap } from '../clients.config';
+import { zipController } from './locations/api/location';
+
+global.fetch = require('node-fetch');
 
 const app = express();
 
-app.get('/location/byZip/:zip((\\d+)(?:-\\d+)?)', (req, res) => {
-    res.json({message: 'Hello world!', zip: req.params.zip })
-});
+const logger = new StackLogger(
+    new ConsoleLogger(),
+);
 
-app.listen(9933, () => console.info('API server started'));
+const locationCtrl = new ResponderFactory(clientMap[Domains.Location], logger);
+const weatherCtrl = new ResponderFactory(clientMap[Domains.Weather], logger);
+
+app.get('/location/byZip/:zip((\\d+)(?:-\\d+)?)', locationCtrl.getResponder(zipController));
+
+app.listen(9933, () => logger.info('API gateway listening on port 9933'));
